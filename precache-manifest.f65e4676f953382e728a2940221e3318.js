a3581ff96c3c3af1cbe5351f59355cd23cdacdbc
Shavlovsii_Ivan <na613c613c@gmail.com>
2020-10-16 20:01:52 +0300

    Updates

---
+++ precache-manifest.f65e4676f953382e728a2940221e3318.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9efdc419243532975e4c7d31e6776301",
+    "url": "/slider/index.html"
+  },
+  {
+    "revision": "f2333b2e07bb2cb48353",
+    "url": "/slider/static/css/main.8a938be5.chunk.css"
+  },
+  {
+    "revision": "d748b3a64316bbd045fa",
+    "url": "/slider/static/js/2.5f8e7e8f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/slider/static/js/2.5f8e7e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2333b2e07bb2cb48353",
+    "url": "/slider/static/js/main.03904c80.chunk.js"
+  },
+  {
+    "revision": "b04782f7968fe3b56b67",
+    "url": "/slider/static/js/runtime-main.3ede3ce7.js"
+  },
+  {
+    "revision": "e1c5226f90ab9d7004598854cf117ecd",
+    "url": "/slider/static/media/ArrowLeft.e1c5226f.svg"
+  },
+  {
+    "revision": "d86e91f63134a9e1fffcd70b120d63ab",
+    "url": "/slider/static/media/ArrowRight.d86e91f6.svg"
+  }
+]);
